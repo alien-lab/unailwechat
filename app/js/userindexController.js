30c2ff7742344af7a7dd602e32792d86fe7486c4
AudreyDan <251918489@qq.com>
2017-08-11 14:54:42 +0800

    创建unailwechat微信端项目

---
+++ app/js/userindexController.js
@@ -0,0 +1,11 @@
+/**
+ * Created by ASUS on 2017/8/8.
+ */
+(function(){
+    'use strict';
+    var app=angular.module("alienlab");
+    app.controller("userindexController",["$scope", function($scope){
+
+        }]);
+
+})();
